cde967948d448afc07833002946cd03cd91b12d4
shaun <dukenan006@163.com>
2021-03-01 11:08:31 +0800

    fix(settings): Modify configuration file

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 import os
 
-DB_NAME = os.getenv('DB_NAME')
-DB_HOST = os.getenv('DB_HOST')
-DB_USER = os.getenv('DB_USER')
-DB_PWD = os.getenv('DB_PWD')
-DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME', 'stockInfo')
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
+DB_USER = os.getenv('DB_USER', 'root')
+DB_PWD = os.getenv('DB_PWD', 'root')
+DB_PORT = os.getenv('DB_PORT', 3306)
 
 MYSQL_DB = f'mysql+pymysql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4'
 
@@ -17,4 +17,4 @@ SQLALCHEMY_ENGINE_CONFIG = {
     'pool_recycle': 3600,
 }
 
-API_TOKEN = os.getenv('API_TOKEN')
+API_TOKEN = os.getenv('API_TOKEN', 'XXXXXXXX')
